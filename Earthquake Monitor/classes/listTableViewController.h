3a0027d922c0213b4a2c8c3672e280d8680363a5
Cesar Saucedo <cesarsaucedo@MacBook-Pro-de-Cesar-2.local>
2015-06-22 21:13:57 -0500

    add the list vc

---
+++ Earthquake Monitor/classes/listTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  listTableViewController.h
+//  Earthquake Monitor
+//
+//  Created by Cesar Saucedo on 22/06/15.
+//  Copyright (c) 2015 César Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface listTableViewController : UITableViewController
+
+@end

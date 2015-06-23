2c7ec9bfd2bc2a0c511ed0cdbe721273d342c51d
Cesar Saucedo <cesarsaucedo@MacBook-Pro-de-Cesar-2.local>
2015-06-22 20:42:14 -0500

    Initial Commit

---
+++ Earthquake Monitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Earthquake Monitor
+//
+//  Created by Cesar Saucedo on 22/06/15.
+//  Copyright (c) 2015 César Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
